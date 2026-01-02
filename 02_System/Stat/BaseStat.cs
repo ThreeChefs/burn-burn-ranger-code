7c71acc1d21caa52d31ce818be2b133a82c1692a
shin0112 <wnslcosltimo12@gmail.com>
2026-01-02 20:54:51 +0900

    feat: 스탯 베이스 가져오기

---
+++ 02_System/Stat/BaseStat.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+
+public class BaseStat
+{
+    // 필드
+    protected StatType type;
+
+    public float BaseValue { get; protected set; }
+    public float CurValue { get; protected set; }
+
+    public virtual float MaxValue => BaseValue;
+
+    // 이벤트
+    public Action<float> OnCurValueChanged;
+
+    /// <summary>
+    /// [public] Stat 보유 클래스가 사라질 경우 메모리 정리를 위해 호출
+    /// </summary>
+    public virtual void OnDestroy()
+    {
+        OnCurValueChanged = null;
+    }
+
+    /// <summary>
+    /// [public] 특정 값 만큼 스텟 값 사용 시도. 변경 다음 OnValueChanged 호출
+    /// </summary>
+    /// <param name="amount"></param>
+    /// <returns></returns>
+    public virtual bool TryUse(float amount)
+    {
+        if (CurValue < amount)
+        {
+            Logger.Log($"{type} 부족");
+            return false;
+        }
+
+        CurValue -= amount;
+        OnCurValueChanged?.Invoke(CurValue);
+        return true;
+    }
+
+    /// <summary>
+    /// [public] 특정 값 만큼 획득
+    /// </summary>
+    /// <param name="amount"></param>
+    public virtual void Add(float amount)
+    {
+        CurValue = Mathf.Min(CurValue + amount, MaxValue);
+        OnCurValueChanged?.Invoke(CurValue);
+    }
+}
---
+++ 02_System/Stat/PlayerStat.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+
+/// <summary>
+/// 스탯 클래스
+/// </summary>
+public class PlayerStat : BaseStat
+{
+    // 필드
+    public float EquipmentValue { get; private set; }
+    public float BuffValue { get; private set; }
+
+    public override float MaxValue => BaseValue + EquipmentValue + BuffValue;
+
+    // 이벤트
+    public event Action<float> OnMaxValueChanged;
+
+    public PlayerStat(float value, StatType type)
+    {
+        BaseValue = value;
+        CurValue = value;
+        this.type = type;
+    }
+
+    /// <summary>
+    /// [public] Stat 보유 클래스가 사라질 경우 메모리 정리를 위해 호출
+    /// </summary>
+    public override void OnDestroy()
+    {
+        base.OnDestroy();
+        OnMaxValueChanged = null;
+    }
+
+    /// <summary>
+    /// [public] 장비 아이템 착용으로 변경된 스텟 값 적용
+    /// </summary>
+    /// <param name="value"></param>
+    public void UpdateEquipmentValue(float value)
+    {
+        EquipmentValue = value;
+
+        CurValue = Mathf.Min(CurValue, MaxValue);
+
+        OnCurValueChanged?.Invoke(CurValue);
+        OnMaxValueChanged?.Invoke(MaxValue);
+    }
+}
