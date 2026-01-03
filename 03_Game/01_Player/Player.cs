1e7127cdc6b62ccec3d9ce5251d07a85d9667596
shin0112 <wnslcosltimo12@gmail.com>
2026-01-03 16:16:50 +0900

    feat: 플레이어 움직임 만들기

--- 03_Game/01_Player/Player.cs
+++ 03_Game/01_Player/Player.cs
@@ -1,15 +1,41 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary>
 /// 플레이어
 /// </summary>
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class Player : MonoBehaviour
 {
-    // 필드
-
+    [Header("움직임")]
+    [SerializeField] private float _speed = 5f;
+    private Vector2 _inputVector;
 
     private void Awake()
     {
 
     }
+    public void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        Vector2 nextVec = _inputVector.normalized * _speed * Time.fixedDeltaTime;
+        Vector2 pos = transform.position;
+        Vector2 newPos = pos + nextVec;
+        transform.position = newPos;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        _inputVector = value.Get<Vector2>();
+    }
+    #region Move
+    #endregion
+
+    #region 에디터 전용
+
+    #endregion
 }
