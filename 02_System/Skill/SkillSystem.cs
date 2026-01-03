0506cf222095221df4ecb19e73f9128caf04881b
shin0112 <wnslcosltimo12@gmail.com>
2026-01-03 14:37:38 +0900

    rename: 이름 변경

--- 02_System/Skill/SkillSystem.cs
+++ 02_System/Skill/SkillSystem.cs
@@ -2,27 +2,32 @@
 using UnityEngine;
 
 /// <summary>
-/// 스킬을 저장하고 관리하는 컨트롤러
+/// 스킬 선택 및 부여를 관리하는 시스템
 /// </summary>
-public class SkillController : MonoBehaviour
+public class SkillSystem : MonoBehaviour
 {
     [SerializeField] private SoDatabase _skillDatabase;
     private List<SkillData> _cache;
 
     // 스킬 상태 관리
     protected readonly List<BaseSkill> havingSkills = new();
-    private BaseSkill _curSkill = null;
 
     private void Awake()
     {
         _cache = _skillDatabase.GetDatabase<SkillData>();
     }
 
+    // todo list
+    // 1. 스킬 선택하기
+    // - 등급과 설명 보여주기
+    // -- 보유 스킬일 경우 / 보유하지 않을 경우 
+    // 2. 스킬 보관하기
+    // 
+
     /// <summary>
     /// [public] 스킬 획득
     /// </summary>
     /// <param name="id"></param>
-    /// <param name="baseSkill"></param>
     public bool TrySelectSkill<T>(int id) where T : BaseSkill
     {
         if (_cache.Count < id)
@@ -33,7 +38,7 @@ public class SkillController : MonoBehaviour
 
         BaseSkill find = havingSkills.Find(skill => skill == _cache[id]);
 
-        if (havingSkills)
+        if (find != null)
         {
             Logger.LogWarning("이미 존재하는 스킬");
             return false;
@@ -44,36 +49,9 @@ public class SkillController : MonoBehaviour
         havingSkills.Add(baseSkill);
 
         baseSkill.Init(_cache[id]);
-        baseSkill.OnStartSkill += HandleStartSkill;
-        baseSkill.OnEndSkill += HandleEndSkill;
 
         return true;
     }
-
-    #region curSkill 관리
-    /// <summary>
-    /// 스킬 사용 시 curSkill에 값 저장
-    /// </summary>
-    /// <param name="baseSkill"></param>
-    private bool HandleStartSkill(BaseSkill baseSkill)
-    {
-        if (_curSkill != null)
-        {
-            Logger.LogWarning("이미 다른 스킬 사용 중");
-            return false;
-        }
-        _curSkill = baseSkill;
-        return true;
-    }
-
-    /// <summary>
-    /// 스킬 사용 완료 시 curSkill에서 값 제거
-    /// </summary>
-    private void HandleEndSkill()
-    {
-        _curSkill = null;
-    }
-    #endregion
 
     #region 에디터 전용
 #if UNITY_EDITOR
@@ -81,7 +59,7 @@ public class SkillController : MonoBehaviour
     {
         if (_skillDatabase == null)
         {
-            _skillDatabase = AssetLoader.FindAndLoadByName<SkillDatabase>("SkillDatabase");
+            _skillDatabase = AssetLoader.FindAndLoadByName<SoDatabase>("PlayerSkillDatabase");
         }
     }
 #endif
