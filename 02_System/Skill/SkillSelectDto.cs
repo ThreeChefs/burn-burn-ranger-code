349dd168028a7b994c17863bc72b11b824d6ec5e
shin0112 <wnslcosltimo12@gmail.com>
2026-01-04 14:54:22 +0900

    feat: 스킬 선택 시 보여줄 Dto 만들기

---
+++ 02_System/Skill/SkillSelectDto.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+/// <summary>
+/// 스킬 선택 시 보여줄 데이터 Dto
+/// </summary>
+public sealed class SkillSelectDto
+{
+    public int Id { get; }
+    public int CurLevel { get; }
+    public string Name { get; }
+    public string Description { get; }
+    public Sprite Icon { get; }
+    public Sprite[] CombinationIcons { get; }
+
+    public SkillSelectDto(
+        int id,
+        int curLevel,
+        string name,
+        string description,
+        Sprite icon,
+        Sprite[] combinationIcons)
+    {
+        Id = id;
+        CurLevel = curLevel;
+        Name = name;
+        Description = description;
+        Icon = icon;
+        CombinationIcons = combinationIcons;
+    }
+}
