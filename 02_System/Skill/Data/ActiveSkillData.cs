db870b270c006a84be691f10bd10d2e6f27b4ab3
shin0112 <wnslcosltimo12@gmail.com>
2026-01-05 17:46:14 +0900

    feat: 액티브 스킬 데이터에 투사체 정보 추가하기
    - 투사체 타입
    - 관통 횟수

--- 02_System/Skill/Data/ActiveSkillData.cs
+++ 02_System/Skill/Data/ActiveSkillData.cs
@@ -6,6 +6,10 @@ public class ActiveSkillData : SkillData
     [field: Header("액티브 스킬 정보")]
     [field: Tooltip("쿨타임")]
     [field: SerializeField] public float Cooldown { get; private set; }
+    [field: Tooltip("탄환 종류")]
+    [field: SerializeField] public ProjectileType ProjectileType { get; private set; }
+    [field: Tooltip("관통 횟수(-1: 무제한 / 0: 아님 / n: 횟수")]
+    [field: SerializeField] public int PassCount { get; private set; }
     [field: Tooltip("탄환 개수")]
     [field: SerializeField] public int[] ProjectilesCounts { get; private set; } = new int[Define.SkillMaxLevel];
     [field: Tooltip("탄환 오브젝트")]
