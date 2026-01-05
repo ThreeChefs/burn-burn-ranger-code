444ae0907e1d49ce7028cad7f96422f7cc666000
SuraJeon <sura6856@naver.com>
2026-01-05 22:37:22 +0900

    feat: BaseUI 기본 동작 추가

--- 10_UI/BaseUI.cs
+++ 10_UI/BaseUI.cs
@@ -1,8 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BaseUI : MonoBehaviour
 {
+    public event Action OnEnableAction;
+    public event Action OnDisableAction;
+    
+    void OnEnable()
+    {
+        OnEnableAction?.Invoke();
+    }
+    
+    
+
+    void OnDisable()
+    {
+        OnDisableAction?.Invoke();
+    }
+
+
+    // todo 애니메이션 넣게 되면 UI 이벤트 안되게 처리도 필요
+    public void OpenUI()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void CloseUI()
+    {
+        // todo 뿅하고 꺼진다던가
+        this.gameObject.SetActive(false);
+    }
     
 }
