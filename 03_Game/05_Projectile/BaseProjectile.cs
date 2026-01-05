63f69404985ed4f835e5a75fcb554b6e24b2bce7
shin0112 <wnslcosltimo12@gmail.com>
2026-01-05 19:10:54 +0900

    feat: 기본 투사체 정보 틀 잡기

--- 03_Game/05_Projectile/BaseProjectile.cs
+++ 03_Game/05_Projectile/BaseProjectile.cs
@@ -1,17 +1,47 @@
 using UnityEngine;
 
-public class BaseProjectile : BasePool
+public class BaseProjectile : BasePool, IAttackable
 {
+    protected ActiveSkill skill;
     protected ProjectileType type;
     protected Vector2 offset;
+    protected float[] levelValue;
 
-    public virtual void Init(ProjectileType type)
+    protected PlayerStat attack;
+
+    protected IDamageable target;
+
+    #region Unity API
+    private void Start()
     {
-        this.type = type;
+        attack = PlayerManager.Instance.Condition[StatType.Attack];
+    }
+    #endregion
+
+    #region 초기화
+    public virtual void Init(ActiveSkill skill, ActiveSkillData data)
+    {
+        this.skill = skill;
+        type = data.ProjectileType;
+        offset = data.Offset;
     }
 
     public virtual void Spawn(Vector2 pos)
     {
         transform.position = pos + offset;
+        target = StageManager.Instance.GetNearestMonster();
     }
+    #endregion
+
+    #region 공격
+    public void Attack(IDamageable damageable)
+    {
+        damageable.TakeDamage(CalculateDamage());
+    }
+
+    protected virtual float CalculateDamage()
+    {
+        return attack.CurValue * levelValue[skill.CurLevel - 1];
+    }
+    #endregion
 }
