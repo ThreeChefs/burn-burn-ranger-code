e2bad3fddb4a7b5ca45380f74baf5c9dd00c01e8
shin0112 <wnslcosltimo12@gmail.com>
2026-01-05 17:50:31 +0900

    feat: 패시브 스킬 획득 시 플레이어한테 수치 적용

--- 02_System/Skill/PassiveSkill.cs
+++ 02_System/Skill/PassiveSkill.cs
@@ -1,3 +1,30 @@
 public class PassiveSkill : BaseSkill
 {
+    private PassiveSkillData _passiveSkillData;
+
+    public override void Init(SkillData data)
+    {
+        base.Init(data);
+
+        _passiveSkillData = data as PassiveSkillData;
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        PlayerManager.Instance.Condition[_passiveSkillData.StatType]
+            .UpdateBuffValue(0);
+    }
+
+    /// <summary>
+    /// [public] 레벨업
+    /// 패시브 스킬이 적용되는 스텟에 해당 level value 데이터 저장
+    /// </summary>
+    public override void LevelUp()
+    {
+        base.LevelUp();
+
+        PlayerManager.Instance.Condition[_passiveSkillData.StatType]
+            .UpdateBuffValue(_passiveSkillData.LevelValue[CurLevel - 1]);
+    }
 }
