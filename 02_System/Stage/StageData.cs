6fd5e69560b4bad9b46a0acb8c2245741aa3c1c5
SuraJeon <sura6856@naver.com>
2026-01-05 19:04:48 +0900

    feat: StageData reward 변수 추가

--- 02_System/Stage/StageData.cs
+++ 02_System/Stage/StageData.cs
@@ -5,8 +5,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "StageData", menuName = "SO/Stage/Stage Data")]
 public class StageData : ScriptableObject
 {
-    public List<StageWaveEntry> StageWaves => _stageWaves;
+    [Title("Reward Data")]
+    [SerializeField] private int _rewardExp = 100;      // 보상 경험치
+    [SerializeField] private int _rewardGold = 100;     // 보상 골드
+    [SerializeField] private int _rewardBoxCount = 5;   // 랜덤으로 장비 업그레이드 스크롤 나오는 갯수
+    
+    [Title("Map Data")]
+    // todo 맵 어떻게 넣을지 고민하기
+    
+    [Title("Wave Data")]
     [SerializeField] List<StageWaveEntry> _stageWaves = new List<StageWaveEntry>();
+
+    
+    
+    
+    public List<StageWaveEntry> StageWaves => _stageWaves;
+    
+    public int RewardExp => _rewardExp;
+    public int RewardGold => _rewardGold;
+    public int RewardBoxCount => _rewardBoxCount;
 
 
     private void OnValidate()
