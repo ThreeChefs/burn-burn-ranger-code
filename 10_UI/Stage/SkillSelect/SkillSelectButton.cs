9cdf1a57f14b4105b57a940103fc2bfda764df5c
SuraJeon <sura6856@naver.com>
2026-01-06 10:34:29 +0900

    feat: SkillSelectButton 버튼정보 세팅, 스킬선택 추가

--- 10_UI/Stage/SkillSelect/SkillSelectButton.cs
+++ 10_UI/Stage/SkillSelect/SkillSelectButton.cs
@@ -7,24 +7,39 @@ using UnityEngine.UI;
 
 public class SkillSelectButton : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI _titleText;
+    [SerializeField] private TextMeshProUGUI _nameText;
     [SerializeField] private TextMeshProUGUI _descriptionText;
     [SerializeField] private Image _headerImg;
     [SerializeField] private Image _iconImg;
     
-    Button _button;
+    private Button _button;
     
-    SkillSelectUI _skillSelectUI;
+    private SkillSelectUI _skillSelectUI;
+    private SkillSelectDto _nowSkillData;
 
     private void Start()
     {
         _button = GetComponent<Button>();
+        _button.onClick.AddListener(SelectSkill);
         _skillSelectUI = GetComponentInParent<SkillSelectUI>();
     }
 
     public void SetSkillButton(SkillSelectDto skillData)
     {
-        _skillSelectUI.CloseUI();
+        _nowSkillData = skillData;
+
+        _nameText.text = skillData.Name;
+        _descriptionText.text = skillData.Description;
+        _iconImg.sprite = skillData.Icon;
+        
+        // todo : 헤더 이미지 색 달라지기
+
+    }
+    
+    void SelectSkill()
+    {
+        StageManager.Instance.SkillSystem.TrySelectSkill(_nowSkillData.Id);
+        _skillSelectUI?.CloseUI();
     }
     
     
