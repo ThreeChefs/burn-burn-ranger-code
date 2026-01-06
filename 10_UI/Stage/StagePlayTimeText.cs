dd550030eabb4b788f25b925f38847969c44a48b
SuraJeon <sura6856@naver.com>
2026-01-06 19:33:10 +0900

    rename: PlayTime 스크립트 이름 변경

--- 10_UI/Stage/StagePlayTimeText.cs
+++ 10_UI/Stage/StagePlayTimeText.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class StagePlayTimeUI : MonoBehaviour
+public class StagePlayTimeText : MonoBehaviour
 {
     TextMeshProUGUI text;
     
