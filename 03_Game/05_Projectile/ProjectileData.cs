204963a1470d3051f98bb7bddbaee06a13844941
shin0112 <wnslcosltimo12@gmail.com>
2026-01-06 14:05:35 +0900

    feat: 투사체 SO 만들기

--- 03_Game/05_Projectile/ProjectileData.cs
+++ 03_Game/05_Projectile/ProjectileData.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New ProjectileData", menuName = "SO/Projectile")]
 public class ProjectileData : ScriptableObject
 {
+    [field: Header("공통")]
     [field: Tooltip("공격력 계수")]
     [field: SerializeField] public float DamageMultiplier { get; private set; }
     [field: Tooltip("탄환 스피드")]
@@ -18,10 +19,15 @@ public class ProjectileData : ScriptableObject
     [field: SerializeField] public LayerMask TargetLayerMask { get; private set; }
 
     // 유도
-    [ShowIf("유도", ProjectileType.Guidance)]
+    [field: Header("유도")]
+    [field: ShowIf("ProjectileType", ProjectileType.Guidance)]
     [field: Tooltip("유도 성능(유도 시간")]
     [field: SerializeField] public float GuidanceTime { get; private set; }
 
+    [field: Header("반사")]
+    [field: ShowIf("ProjectileType", ProjectileType.Reflection)]
+    [field: Tooltip("반사 타겟 레이어")]
+    [field: SerializeField] public LayerMask ReflectionLayerMask { get; private set; }
 
 #if UNITY_EDITOR
     private void Reset()
--- 03_Game/05_Projectile/ProjectileEnums.cs
+++ 03_Game/05_Projectile/ProjectileEnums.cs
@@ -1,6 +1,7 @@
 public enum ProjectileType
 {
-    Chase,      // 단순 추격
-    Hover,      // 범위
-    Guidance    // 유도 추격
+    Chase,          // 단순 추격
+    Hover,          // 범위
+    Guidance,       // 유도 추격
+    Reflection,     // 반사
 }
