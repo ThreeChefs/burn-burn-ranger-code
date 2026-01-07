2731e9e3f26a603e024d333c0df5de558db6d4db
SuraJeon <sura6856@naver.com>
2026-01-07 21:28:40 +0900

    feat: VictoryUI 프리팹, 스크립트 추가

---
+++ 10_UI/Stage/VictoryUI.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+public class VictoryUI : BaseUI
+{
+    [SerializeField] private TextMeshProUGUI _stageNumberText;
+    [SerializeField] private TextMeshProUGUI _killCountText;
+    [SerializeField] private TextMeshProUGUI _timeText;
+    
+    
+    public override void OpenUIInternal()
+    {
+    }
+
+    public override void CloseUIInternal()
+    {
+    
+    }
+}
