5ff95b8fb1b8b2aaaf2bba008f73c0fd4ee24b4b
sooroora <sura6856@naver.com>
2026-01-08 00:51:24 +0900

    fix: 결과 UI 프리팹 연결 수정

--- 10_UI/Stage/StageResultUI.cs
+++ 10_UI/Stage/StageResultUI.cs
@@ -9,15 +9,15 @@ public class StageResultUI : BaseUI
     [SerializeField] private TextMeshProUGUI _killCountText;
     [SerializeField] private TextMeshProUGUI _timeText;
     [SerializeField] private Button _nextButton;
-    
+
     public void Start()
     {
-        _stageNumberText.text = StageManager.Instance.NowStageNumber.ToString();
+        _stageNumberText.text = (StageManager.Instance.NowStageNumber + 1).ToString();
         _killCountText.text = StageManager.Instance.KillCount.ToString();
-        
+
         TimeSpan t = TimeSpan.FromSeconds(StageManager.Instance.PlayTime);
         _timeText.text = t.ToString(@"mm\:ss");
-        
+
         _nextButton.onClick.AddListener(NextScene);
     }
 
