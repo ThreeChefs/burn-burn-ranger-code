f736d48c864f9baa590a3f7b5da3c1ce93a63c3a
shin0112 <wnslcosltimo12@gmail.com>
2026-01-08 12:17:51 +0900

    feat: item box data를 entry로 분리해서 클래스, 가중치 넣기

--- 03_Game/04_Item/ItemBoxData.cs
+++ 03_Game/04_Item/ItemBoxData.cs
@@ -6,6 +6,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New ItemBoxData", menuName = "SO/Item/Item Box")]
 public class ItemBoxData : ScriptableObject
 {
+    [field: SerializeField] public List<ItemBoxEntry> Items { get; private set; }
+}
+
+[System.Serializable]
+public class ItemBoxEntry
+{
+    [field: SerializeField] public ItemClass ItemClass { get; private set; }
+    [field: SerializeField] public int Weight { get; private set; }
     [field: SerializeField] public List<ItemData> Items { get; private set; }
 
 #if UNITY_EDITOR
