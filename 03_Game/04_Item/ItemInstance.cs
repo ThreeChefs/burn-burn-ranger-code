fcb851da1b5576a5e9f6ca757fc7754dc96fc70d
shin0112 <wnslcosltimo12@gmail.com>
2026-01-08 14:50:26 +0900

    feat: 아이템 instance에 레벨, 개수 정보 포함

--- 03_Game/04_Item/ItemInstance.cs
+++ 03_Game/04_Item/ItemInstance.cs
@@ -4,13 +4,21 @@
 public class ItemInstance
 {
     public ItemClass ItemClass { get; private set; }
-
     public ItemData ItemData { get; private set; }
+    public int Count { get; private set; }
+    public int Level { get; private set; }
 
+    /// <summary>
+    /// [public] 생성자
+    /// </summary>
+    /// <param name="itemClass"></param>
+    /// <param name="itemData"></param>
     public ItemInstance(ItemClass itemClass, ItemData itemData)
     {
         ItemClass = itemClass;
         ItemData = itemData;
+        Count = 1;
+        Level = 1;
     }
 
     public override string ToString()
