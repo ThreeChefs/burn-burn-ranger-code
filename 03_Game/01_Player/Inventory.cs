ace2c0c6257a2dfb2226099e41356a6766f0f6f9
shin0112 <wnslcosltimo12@gmail.com>
2026-01-08 15:19:47 +0900

    feat: 인벤토리 변경 시 이벤트로 갱신하기

--- 03_Game/01_Player/Inventory.cs
+++ 03_Game/01_Player/Inventory.cs
@@ -1,20 +1,23 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 /// <summary>
 /// 인벤토리
 /// </summary>
-[System.Serializable]
 public class Inventory
 {
     private List<ItemInstance> _items = new();
     public IReadOnlyList<ItemInstance> Items => _items;
+
+    public event Action OnInventoryChanged;
 
     /// <summary>
     /// [public] 플레이어가 파괴될 때 실행
     /// </summary>
     public void OnDestroy()
     {
+        OnInventoryChanged = null;
         // todo: 아이템 데이터 저장
     }
 
@@ -25,6 +28,7 @@ public class Inventory
     public void Add(ItemInstance item)
     {
         _items.Add(item);
+        OnInventoryChanged?.Invoke();
     }
 
     /// <summary>
@@ -34,6 +38,7 @@ public class Inventory
     public void Remove(ItemInstance item)
     {
         _items.Remove(item);
+        OnInventoryChanged?.Invoke();
     }
 
     // todo: Comparer 정렬으로 변경
