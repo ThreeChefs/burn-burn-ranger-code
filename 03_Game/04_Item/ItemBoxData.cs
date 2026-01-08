7e0c13ed1c45e2070b0e30ad5307dbd99b27fca5
shin0112 <wnslcosltimo12@gmail.com>
2026-01-08 11:48:43 +0900

    feat: 아이템 박스 SO 정렬 만들고, 테스트에 쓸 아이템 박스 SO 작성

--- 03_Game/04_Item/ItemBoxData.cs
+++ 03_Game/04_Item/ItemBoxData.cs
@@ -1,8 +1,20 @@
+using Sirenix.OdinInspector;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New ItemBoxData", menuName = "SO/Item/Item Box")]
 public class ItemBoxData : ScriptableObject
 {
     [field: SerializeField] public List<ItemData> Items { get; private set; }
+
+#if UNITY_EDITOR
+    [Button("정렬")]
+    private void SortItems()
+    {
+        Items = Items.OrderBy(item => item.EquipmentType)
+            .ThenBy(item => item.Id)
+            .ToList();
+    }
+#endif
 }
