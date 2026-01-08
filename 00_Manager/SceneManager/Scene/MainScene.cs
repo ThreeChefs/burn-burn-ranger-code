bb3df3a36ca13320d90d8e7c95be13ab7e8f11c4
shin0112 <wnslcosltimo12@gmail.com>
2026-01-08 23:22:05 +0900

    feat: shop ui 메인 씬에 추가하기

--- 00_Manager/SceneManager/Scene/MainScene.cs
+++ 00_Manager/SceneManager/Scene/MainScene.cs
@@ -4,6 +4,7 @@
     {
         UIManager.Instance?.SpawnUI(UIName.UI_StageSelect);
         UIManager.Instance.LoadUI(UIName.UI_Equipment);
+        UIManager.Instance.LoadUI(UIName.UI_Shop);
         UIManager.Instance.SpawnUI(UIName.UI_Home);
     }
 }
