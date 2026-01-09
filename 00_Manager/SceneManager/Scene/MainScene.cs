c65997d3e7400c98b29545d56289478921e57ffa
SuraJeon <sura6856@naver.com>
2026-01-09 10:20:59 +0900

    feat: MainScene UI Load 수정

--- 00_Manager/SceneManager/Scene/MainScene.cs
+++ 00_Manager/SceneManager/Scene/MainScene.cs
@@ -2,10 +2,12 @@
 {
     private void Start()
     {
-        UIManager.Instance?.SpawnUI(UIName.UI_StageSelect);
+        //UIManager.Instance.SpawnUI(UIName.UI_StageSelect);
+        UIManager.Instance.LoadUI(UIName.UI_HomeBottomMenu);
+
+        UIManager.Instance.LoadUI(UIName.UI_Home);
         UIManager.Instance.LoadUI(UIName.UI_Equipment, false);
         UIManager.Instance.LoadUI(UIName.UI_Shop, false);
         UIManager.Instance.LoadUI(UIName.UI_PickUp, false);
-        UIManager.Instance.SpawnUI(UIName.UI_Home);
     }
 }
