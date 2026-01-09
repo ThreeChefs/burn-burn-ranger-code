3103d94f4d306ad7538e4cec9fcd7138e794f6be
SuraJeon <sura6856@naver.com>
2026-01-09 12:43:22 +0900

    feat: PopupUI 동작 추가

--- 10_UI/BaseUI.cs
+++ 10_UI/BaseUI.cs
@@ -1,6 +1,8 @@
 using Sirenix.OdinInspector;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class BaseUI : MonoBehaviour
 {
@@ -9,12 +11,20 @@ public abstract class BaseUI : MonoBehaviour
     [SerializeField] bool _isSubCanvas = false;
     public bool IsSubCanvas => _isSubCanvas;
 
-
     public event Action<BaseUI> OnOpenAction;
     public event Action<BaseUI> OnCloseAction;
     public event Action<BaseUI> OnDestroyAction;
+
+    private void Awake()
+    {
+        AwakeInternal();
+    }
     
-    
+    protected virtual void AwakeInternal()
+    {
+
+    }
+
     // todo 애니메이션 넣게 되면 UI 이벤트 안되게 처리도 필요
     public void OpenUI()
     {
