52ef71d8bd0e7689fc0dee22a38c1a29842c52d9
shin0112 <wnslcosltimo12@gmail.com>
2026-01-10 14:03:13 +0900

    feat: 대미지 이펙트 구조체 만들어 놓기

---
+++ 03_Game/00_Common/DamageEffect.cs
@@ -0,0 +1,10 @@
+using System.Numerics;
+
+public struct DamageEffect
+{
+    public float damage;
+    public float tickInterval;
+    public float duration;
+    public Vector2 direction;
+    public float knockbackForce;
+}
