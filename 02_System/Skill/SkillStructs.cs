e6824335efb7a5d863a05fbcf13de1d2734cfb31
shin0112 <wnslcosltimo12@gmail.com>
2026-01-10 17:57:59 +0900

    refactor: 스킬 관련 struct 모음 파일 제작

---
+++ 02_System/Skill/SkillStructs.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public struct DamageEffect
+{
+    public float damage;
+    public float tickInterval;
+    public float duration;
+    public Vector2 direction;
+    public float knockbackForce;
+}
+
+/// <summary>
+/// 피격 시 필요한 정보를 담은 struct
+/// 읽기 전용 참조용
+/// </summary>
+public struct HitContext
+{
+    public Transform attacker;
+    public Vector2 position;
+    public Collider2D directTarget;
+    public ProjectileData projectileData;
+}
