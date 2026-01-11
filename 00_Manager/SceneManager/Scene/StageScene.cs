ccbe4547bb5ef93947a86819df49870621e27f96
shin0112 <wnslcosltimo12@gmail.com>
2026-01-11 17:10:09 +0900

    feat: 로켓 투사체 pool에 올리기

--- 00_Manager/SceneManager/Scene/StageScene.cs
+++ 00_Manager/SceneManager/Scene/StageScene.cs
@@ -1,19 +1,15 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class StageScene : BaseScene
 {
     private void Start()
     {
-        
+
         // todo : 임시! 플레이어, 스테이지 정보 읽고 사용될 수 있는 것들을 쭉 등록해줘야함.
         ProjectileManager.Instance.UsePool(ProjectileDataIndex.KunaiProjectileData);
         ProjectileManager.Instance.UsePool(ProjectileDataIndex.RangedAttack);
         ProjectileManager.Instance.UsePool(ProjectileDataIndex.FireBombProjectileData);
-        
-        
+        ProjectileManager.Instance.UsePool(ProjectileDataIndex.RocketProjectileData);
+
+
         CommonPoolManager.Instance.UsePool(CommonPoolIndex.DamageText);
     }
 }
