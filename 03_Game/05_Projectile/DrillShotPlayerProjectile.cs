e09bfb703097fe6decba036f858ce7fdddf75220
shin0112 <wnslcosltimo12@gmail.com>
2026-01-13 21:29:25 +0900

    feat: 스킬 드릴샷 - 벽에 반사되면 회전되게하기

---
+++ 03_Game/05_Projectile/DrillShotPlayerProjectile.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+/// <summary>
+/// 투사체 - 드릴샷
+/// </summary>
+public class DrillShotPlayerProjectile : PlayerProjectile
+{
+    protected override void HandleScreenReflection()
+    {
+        base.HandleScreenReflection();
+
+        transform.rotation = Quaternion.Euler(0f, 0f, Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg);
+    }
+}
