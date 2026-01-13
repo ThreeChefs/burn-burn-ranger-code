0e27257488127f12d1164878280dbc9e730f2e81
shin0112 <wnslcosltimo12@gmail.com>
2026-01-13 17:11:20 +0900

    fix: transform nre 문제 방지

--- 02_System/Skill/ActiveSkill/QuantumBallActiveSkill.cs
+++ 02_System/Skill/ActiveSkill/QuantumBallActiveSkill.cs
@@ -16,19 +16,21 @@ public class QuantumBallActiveSkill : ActiveSkill
 
     protected override IEnumerator UseSkill(Transform target = null)
     {
-        prevProjectile = ProjectileManager.Instance
-            .Spawn(projectileIndex, this, transform.forward)
-            .GetComponent<Transform>();
-
-        Logger.Log("대기");
-        yield return projectileSpawnInterval;
-
-        int count = (int)skillValues[SkillValueType.ProjectileCount][CurLevel - 1];
-        int rand = Random.Range(count / 2, count);
-        for (int i = 0; i < rand; i++)
+        target = MonsterManager.Instance.GetNearestMonster();
+        if (target != null)
         {
-            Vector2 dir = Quaternion.Euler(0, 0, Random.Range(0f, 360f)) * prevProjectile.forward;
-            ProjectileManager.Instance.Spawn(projectileIndex, this, dir, prevProjectile.position);
+            prevProjectile = ProjectileManager.Instance.Spawn(projectileIndex, this, target).GetComponent<Transform>();
+
+            Logger.Log("대기");
+            yield return projectileSpawnInterval;
+
+            int count = (int)skillValues[SkillValueType.ProjectileCount][CurLevel - 1];
+            int rand = Random.Range(count / 2, count);
+            for (int i = 0; i < rand; i++)
+            {
+                Vector2 dir = Quaternion.Euler(0, 0, Random.Range(0f, 360f)) * prevProjectile.forward;
+                ProjectileManager.Instance.Spawn(projectileIndex, this, dir, prevProjectile.position);
+            }
         }
     }
 }
