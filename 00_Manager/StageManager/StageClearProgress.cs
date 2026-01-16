212af3978409b12d23fe8dab518345d8e2d25260
SuraJeon <sura6856@naver.com>
2026-01-16 17:49:32 +0900

    feat: GameManager 에 스테이지 진행도 전달

--- 00_Manager/StageManager/StageClearProgress.cs
+++ 00_Manager/StageManager/StageClearProgress.cs
@@ -4,16 +4,17 @@ using System;
 // StageClearProgress 
 public class StageClearProgress
 {
-    int _clearStageNum = 0;             // 현재 클리어한 스테이지 번호
+    int _clearStageNum = 0;             // 현재 클리어한 스테이지 번호 / 1스테이지 클리어 한 상태라면 1로 저장된 상태
 
     int _openedStageRewardStep = 0;     // 열린 리워드 수
     int _receivedStageRewardStep = 0;   // 리워드 어디까지 받았는지
 
-    float _lastPlayingStageRecord = 0f; // 클리어하지 못한 마지막 스테이지 기록 (00:00. 08:00 ... 등)
+    int _lastPlayingStageRecord = 0; // 클리어하지 못한 마지막 스테이지 기록 (00:00. 08:00 ... 등)
 
     public int ClearStageNum => _clearStageNum;
     public int OpenedStageRewardStep => _openedStageRewardStep;
     public int ReceivedStageRewardStep => _receivedStageRewardStep; 
+    public int LastPlayingStageRecord => _lastPlayingStageRecord;
 
 
     public StageClearProgressSave ExportProgress()
@@ -22,12 +23,17 @@ public class StageClearProgress
         {
             clearStageNum = _clearStageNum,
             openedStageRewardStep = _openedStageRewardStep,
-            receivedStageRewardStep = _receivedStageRewardStep
-
+            receivedStageRewardStep = _receivedStageRewardStep,
+            lastPlayingStageRecord = _lastPlayingStageRecord,
         };
     }
-    
 
+    public void SaveStagePrgress(int stageNum, int lastPlayingStageRecord)
+    {
+        _clearStageNum = stageNum;
+        _lastPlayingStageRecord= lastPlayingStageRecord;
+    }
+    
 }
 
 [Serializable]
@@ -36,6 +42,7 @@ public class StageClearProgressSave
     public int clearStageNum;
     public int openedStageRewardStep;
     public int receivedStageRewardStep;
+    public int lastPlayingStageRecord;
 
 }
 
