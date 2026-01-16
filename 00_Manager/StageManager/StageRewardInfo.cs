acd33576bae660286fa1c667a2539f00bd379181
SuraJeon <sura6856@naver.com>
2026-01-16 16:11:52 +0900

    feat: StageResultInfo, StageRewardInfo 를 StageStruct 스크립트로 분리

--- 00_Manager/StageManager/StageRewardInfo.cs
+++
@@ -1,9 +0,0 @@
-public struct StageRewardInfo               //정보 전달만 하는 용도
-{
-    public ItemType type;
-
-    public ItemInstance itemInfo;
-
-    public WalletType upgradeMaterialType;
-    public int count;
-}
---
+++ 00_Manager/StageManager/StageStruct.cs
@@ -0,0 +1,18 @@
+﻿public struct StageRewardInfo
+{
+    public ItemType type;
+
+    public ItemInstance itemInfo;
+
+    public WalletType upgradeMaterialType;
+    public int count;
+}
+
+public struct StageResultInfo
+{
+    public string stageName;
+    public int killCount;
+    public float playTIme;
+    public float gold;
+    public float exp;
+}
