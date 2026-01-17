4b7ce798c8b034be471033bed126a72871f77b0b
shin0112 <wnslcosltimo12@gmail.com>
2026-01-17 14:34:36 +0900

    feat: 로딩 씬 만들고 progress bar에 적용할 수 있게 하기

--- 00_Manager/SceneManager/SceneController.cs
+++ 00_Manager/SceneManager/SceneController.cs
@@ -20,6 +20,7 @@ public class SceneController
     private string _externalSceneName;
 
     private Coroutine _coroutine;
+    private readonly WaitForSeconds _loadDelay = new(0.3f);
     #endregion
 
     #region 초기화
@@ -114,12 +115,24 @@ public class SceneController
         // todo: 로딩 씬 필요
         string sceneName = _curSceneType == SceneType.None ? _externalSceneName : _curSceneType.ToString();
 
+        var loadingUI = GameObject.FindAnyObjectByType<LoadingUI>();
+        if (loadingUI != null) { loadingUI.OpenUI(); }
+
         AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
-        while (!async.isDone)
+        async.allowSceneActivation = false;
+
+        Logger.Log($"{_curSceneType}으로 로딩 중...");
+        while (async.progress < 0.9f)
         {
+            if (loadingUI != null) { loadingUI.SetProgress(async.progress); }
+            Logger.Log($"진행률: {async.progress}");
             yield return null;
-            Logger.Log($"로딩 상태: {async.progress * 100}%...");
         }
+
+        if (loadingUI != null) { loadingUI.SetProgress(1f); }
+
+        yield return _loadDelay;
+        async.allowSceneActivation = true;
     }
 
     /// <summary>
---
+++ 10_UI/Common/LoadingUI.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingUI : BaseUI
+{
+    [SerializeField] private Image _progress;
+
+    public void SetProgress(float progress)
+    {
+        _progress.fillAmount = progress;
+    }
+
+#if UNITY_EDITOR
+    private void Reset()
+    {
+        _progress = transform.FindChild<Image>("Image - Progress");
+    }
+#endif
+}
