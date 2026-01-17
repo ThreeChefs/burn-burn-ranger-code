1997c39c852c5880e913c2d0322085d968f73d9d
SuraJeon <sura6856@naver.com>
2026-01-17 17:52:46 +0900

    feat: 잠긴 slot 흑백 처리

--- 10_UI/Main/Growth/GrowthSlot.cs
+++ 10_UI/Main/Growth/GrowthSlot.cs
@@ -7,6 +7,9 @@ public class GrowthSlot : BaseSlot
     [Title("성장 슬롯")]
     [SerializeField] Button _slotButton;
 
+    [Title("흑백 Material")]
+    [SerializeField] Material _grayScaleMat;
+
     public GrowthInfo GrowthInfo => _growthInfo;
     GrowthInfo _growthInfo;
 
@@ -15,9 +18,15 @@ public class GrowthSlot : BaseSlot
     public event Action<GrowthSlot, int> OnClickGrowthButtonAction;
 
 
+    Image[] _images;
+    
+
     private void Awake()
     {
         _slotButton.onClick.AddListener(OnClickSlot);
+
+        _images= this.GetComponentsInChildren<Image>();
+        
     }
 
     public void SetSlot(SlotInfo slotInfo, GrowthInfo growthInfo, int unlockCount )
@@ -25,11 +34,31 @@ public class GrowthSlot : BaseSlot
         SetSlot(slotInfo);
         _growthInfo = growthInfo;
         UnlockCount = unlockCount;
+
+        if(GameManager.Instance.GrowthProgress.NormalUnlockCount > unlockCount -1)
+        {
+            SetLockImg(false);
+        }
+        else
+        {
+            SetLockImg(true);
+        }
     }
 
     public void OnClickSlot()
     {
         OnClickGrowthButtonAction?.Invoke(this, UnlockCount);
+    }
+
+    public void SetLockImg(bool locked)
+    {
+        for (int i = 0; i < _images.Length; i++)
+        {
+            if (locked)
+                _images[i].material = _grayScaleMat;
+            else
+                _images[i].material = null;
+        }
     }
 
 }
