094cebd5abd5623a686521a114d4b6ca7adb2d61
곽호빈 <hobin1117@gmail.com>
2026-01-17 16:12:37 +0900

    feat: 보스 레이저 수정

---
+++ 03_Game/02_Monster/BossVfxController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class BossVfxController : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem auraVfx; // 보스에 붙은 파티클
+
+    public void SetAura(bool on)
+    {
+        if (auraVfx == null) return;
+
+        if (on)
+        {
+            auraVfx.gameObject.SetActive(true);   // 꺼져있을 수 있으니
+            auraVfx.Play(true);
+        }
+        else
+        {
+            auraVfx.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+            // 필요하면 완전히 숨김
+            // auraVfx.gameObject.SetActive(false);
+        }
+    }
+}
