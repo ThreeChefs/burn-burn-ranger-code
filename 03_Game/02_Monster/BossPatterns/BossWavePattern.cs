47d2696bdc2536bf859d5aa659a64356af035d6e
곽호빈 <hobin1117@gmail.com>
2026-01-19 20:44:38 +0900

    feat: 아이템 크기 수정

---
+++ 03_Game/02_Monster/BossPatterns/BossWavePattern.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+public class BossWavePattern : BossPatternBase
+{
+    protected override bool CanRun()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override IEnumerator Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+}
