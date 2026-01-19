35d949c232e92976f6d1c0d2420a7a3038045251
SuraJeon <sura6856@naver.com>
2026-01-19 16:07:36 +0900

    feat: 행운상자 추가
    - 먹으면 행운열차 UI 뜰 예정

--- 00_Manager/PoolManager/PoolEnums.cs
+++ 00_Manager/PoolManager/PoolEnums.cs
@@ -1,4 +1,4 @@
-﻿/// <summary>
+/// <summary>
 /// 풀링해서 쓸 오브젝트들의 데이터 인덱스
 /// </summary>
 
@@ -6,6 +6,7 @@ public enum CommonPoolIndex
 {
     DamageText,
     DronAim,
+    FortuneBox,
 }
 
 public enum MonsterPoolIndex
---
+++ 03_Game/03_Stage/Item/FortuneBox.cs
@@ -0,0 +1,47 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FortuneBox : PoolObject
+{
+
+    WaveClearRewardType _rewardType;
+
+    public void Init(WaveClearRewardType rewardType)
+    {
+        _rewardType = rewardType;
+    }
+
+
+    void ShowFortuneBoxUI()
+    {
+        switch (_rewardType)
+        {
+            case WaveClearRewardType.Fortune_Skill_Random:
+                Debug.Log("랜덤행운열차");
+                break;
+            case WaveClearRewardType.Fortune_Skill_1:
+                Debug.Log("스킬 1개 획득");
+                break;
+            case WaveClearRewardType.Fortune_Skill_3:
+                Debug.Log("스킬 3개 획득");
+                break;
+            case WaveClearRewardType.Fortune_Skill_5:
+                Debug.Log("스킬 5개 획득");
+                break;
+        }
+
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == Define.PlayerLayer)
+        {
+            ShowFortuneBoxUI();
+            gameObject.SetActive(false);
+        }
+
+    }
+
+}
