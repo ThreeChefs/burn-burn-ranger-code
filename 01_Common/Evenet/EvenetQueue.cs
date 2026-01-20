7e06581b1c345c3176e3a3e47c8633c1fd5adb34
SuraJeon <sura6856@naver.com>
2026-01-20 22:05:25 +0900

    feat: EventQueue 추가
    - 이벤트 큐 스크립트 추가

---
+++ 01_Common/Evenet/EvenetQueue.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+public sealed class EvenetQueue
+{
+    private readonly Queue<Action<Action>> _queue = new();
+    private bool _running;
+
+    public void Enqueue(Action<Action> action)
+    {
+        _queue.Enqueue(action);
+        TryRunNext();
+    }
+
+    private void TryRunNext()
+    {
+        if (_running) return;
+        if (_queue.Count == 0) return;
+
+        _running = true;
+        var nextAction = _queue.Dequeue();
+
+        nextAction.Invoke(() =>
+        {
+            _running = false;
+            TryRunNext();
+        });
+    }
+
+    public void ClearAll()
+    {
+        _queue.Clear();
+        _running = false;
+    }
+}
