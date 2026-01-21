ece5d260e6fc20cb177d5a84a6abd6fb7be2c323
SuraJeon <sura6856@naver.com>
2026-01-21 17:25:11 +0900

    feat: SpriteSorting enum 으로 하위 스프라이트들 한번에 설정할 수 있게 추가

--- 03_Game/00_Common/SpriteSorting.cs
+++ 03_Game/00_Common/SpriteSorting.cs
@@ -5,9 +5,22 @@ public class SpriteSorting : MonoBehaviour
 {
     SpriteRenderer[] sprites;
 
+    [SerializeField] SpriteSortingLayer _sprSortingLayer;
+
     private void Awake()
     {
-        sprites = GetComponentsInChildren<SpriteRenderer>();
+        sprites = GetComponentsInChildren<SpriteRenderer>(true);
+
+
+        string layerName = GetLayerName(_sprSortingLayer);
+        int layerId = SortingLayer.NameToID(layerName);     // 레이어 ID는 0,1,2.. 가 아니어서 레이어 이름을 가져와서 아이디를 넣어줘야함!
+
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            sprites[i].sortingLayerID = layerId;
+        }
+
+
     }
     
     private void LateUpdate()
@@ -17,4 +30,38 @@ public class SpriteSorting : MonoBehaviour
             sprites[i].sortingOrder = (int)(this.transform.position.y * 1000) * -1;
         }
     }
+
+
+
+    private static string GetLayerName(SpriteSortingLayer layer)
+    {
+        return layer switch
+        {
+            SpriteSortingLayer.Default => "Default",
+            SpriteSortingLayer.BackGround => "BackGround",
+            SpriteSortingLayer.BackGroundDecal => "BackGroundDecal",
+            SpriteSortingLayer.Item => "Item",
+            SpriteSortingLayer.Character => "Character",
+            SpriteSortingLayer.Projectile => "Projectile",
+            SpriteSortingLayer.Effect => "Effect",
+            SpriteSortingLayer.WorldUI => "WorldUI",
+            _ => "Default",     // 이넘 와일드카드
+        };
+    }
+
 }
+
+
+public enum SpriteSortingLayer
+{
+    Unspecified,
+    Default,
+    BackGround,
+    BackGroundDecal,
+    Item,
+    Character,
+    Projectile,
+    Effect,
+    WorldUI,
+}
+
