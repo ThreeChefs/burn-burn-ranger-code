3308ef8c2819609c90372743c39615882f28d386
shin0112 <wnslcosltimo12@gmail.com>
2026-01-27 14:33:38 +0900

    fix: 무기 장비 안 했을 때 nre 해결

--- 02_System/Skill/ActiveSkill/KunaiActiveSkill.cs
+++ 02_System/Skill/ActiveSkill/KunaiActiveSkill.cs
@@ -6,7 +6,8 @@ public class KunaiActiveSkill : ActiveSkill
     private void Start()
     {
         ItemInstance weapon = PlayerManager.Instance.Equipment.Equipments[EquipmentType.Weapon];
-        if (weapon.ItemClass >= ItemClass.Rare)
+
+        if (weapon != null && weapon.ItemClass >= ItemClass.Rare)
         {
             DamageMultiplier = 1.3f;
         }
