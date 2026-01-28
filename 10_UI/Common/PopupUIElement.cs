22818002ed4365261ee71b80ba2803e9b771b69d
SuraJeon <sura6856@naver.com>
2026-01-28 19:01:37 +0900

    feat: PopupUIElement 추가

---
+++ 10_UI/Common/PopupUIElement.cs
@@ -0,0 +1,83 @@
+﻿
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class PopupUIElement : MonoBehaviour
+{
+    [Title("Open")]
+    [SerializeField] PopupUIOpenType _openType;
+    [SerializeField] Ease _openEase = Ease.OutQuad;
+
+    [Title("Close")]
+    [SerializeField] PopupUIOpenType _closeType;
+    [SerializeField] Ease _closeEase = Ease.OutQuad;
+
+    RectTransform _rect;
+    Vector2 _originPos;
+
+    private void Awake()
+    {
+        _rect = GetComponent<RectTransform>();
+        _originPos = _rect.anchoredPosition;
+    }
+
+    public void Open(float _duration)
+    {
+        switch (_openType)
+        {
+
+            case PopupUIOpenType.Default:
+                if (_rect != null)
+                {
+                    _rect.localScale = Vector3.zero;
+                    _rect.DOScale(1f, _duration).SetEase(Ease.OutQuad).SetUpdate(true);
+                }
+                break;
+
+            case PopupUIOpenType.Horizontal:
+                if (_rect != null)
+                {
+                    _rect.localScale = new Vector3(0, 1, 1);
+                    _rect.DOScale(1f, _duration).SetEase(Ease.OutQuad).SetUpdate(true);
+                }
+                break;
+
+            case PopupUIOpenType.Vertical:
+                if (_rect != null)
+                {
+                    _rect.localScale = new Vector3(1, 0, 1);
+                    _rect.DOScale(1f, _duration).SetEase(Ease.OutQuad).SetUpdate(true);
+                }
+                break;
+        }
+    }
+
+    public void Close(float _duration)
+    {
+        switch (_closeType)
+        {
+            case PopupUIOpenType.Default:
+                if (_rect != null)
+                {
+                    return _rect.DOScale(0f, _duration).SetEase(Ease.InQuad).SetUpdate(true);
+                }
+                break;
+
+            case PopupUIOpenType.Horizontal:
+                if (_rect != null)
+                {
+                    return _rect.DOScale(new Vector3(0, 1, 1), _duration).SetEase(Ease.OutQuad).SetUpdate(true);
+                }
+                break;
+
+            case PopupUIOpenType.Vertical:
+                if (_rect != null)
+                {
+                    return _rect.DOScale(new Vector3(1, 0, 1), _duration).SetEase(Ease.OutQuad).SetUpdate(true);
+                }
+                break;
+        }
+    }
+
+}
