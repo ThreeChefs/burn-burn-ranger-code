a0b0ec83ebc32e305191a9e482c355cbf8881eee
SuraJeon <sura6856@naver.com>
2026-01-29 03:19:06 +0900

    feat: SkillDamageDataUI 스크립트 추가

---
+++ 10_UI/Stage/SkillDamageData/SkillDamageDataUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillDamageDataUI : PopupUI
+{
+    [SerializeField] Button _backButton;
+
+    protected override void AwakeInternal()
+    {
+        base.AwakeInternal();
+        _backButton.onClick.AddListener(OnClickBackButton);
+    }
+
+    void OnClickBackButton()
+    {
+        CloseUI();
+    }
+}
