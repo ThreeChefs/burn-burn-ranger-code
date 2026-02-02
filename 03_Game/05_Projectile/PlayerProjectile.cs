a11ea28c5f2bd244dd8b85e0d888a606d019d779
shin0112 <wnslcosltimo12@gmail.com>
2026-02-02 17:37:43 +0900

    style: 로그 주석 처리

--- 03_Game/05_Projectile/PlayerProjectile.cs
+++ 03_Game/05_Projectile/PlayerProjectile.cs
@@ -191,7 +191,7 @@ public class PlayerProjectile : BaseProjectile
         float rand = Random.Range(0f, 1f);
         if (fatalProability.MaxValue * 0.01f > rand)
         {
-            Logger.Log($"치명타! 확률: {rand} / {fatalProability.MaxValue * 0.01f}");
+            //Logger.Log($"치명타! 확률: {rand} / {fatalProability.MaxValue * 0.01f}");
             fatalMultiplier = 2f;
         }
 
